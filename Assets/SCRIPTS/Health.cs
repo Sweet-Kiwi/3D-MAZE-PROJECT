1cfcb5e587ca0bc7e1ea24e8ae3c67fd74889952
Sweet-Kiwi <kyra021206@gmail.com>
2025-03-13 16:31:24 +0100

    You_Died_Screen_and_fade
    You died screen ,animation, fade in and out.

--- Assets/SCRIPTS/Health.cs
+++ Assets/SCRIPTS/Health.cs
@@ -5,7 +5,8 @@ public class Health : MonoBehaviour
     public int points = 5;
     public Vector3 respawnPosition;
     public TMP_Text healthText;
-
+    public EndScreenAnimation gameOverScreen;
+    
     private void Start()
     {
         respawnPosition = transform.position;
@@ -38,6 +39,7 @@ public class Health : MonoBehaviour
            // points = 5;
             if (points < 1)
         {
+            gameOverScreen.StartFade();
            Destroy(gameObject);
         }
     }
